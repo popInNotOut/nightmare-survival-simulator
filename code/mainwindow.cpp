ac4a2dac22ce668954683c63f07b3b2db69b8d4b
Justin Xu <jxu853@uwo.ca>
2024-11-17 22:40:29 -0500

    Disabled fullscreen

--- code/mainwindow.cpp
+++ code/mainwindow.cpp
@@ -10,6 +10,7 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->setWindowFlags(this->windowFlags() & ~Qt::WindowMaximizeButtonHint);
     ui->stackedWidget->setCurrentIndex(0);
 }
 
